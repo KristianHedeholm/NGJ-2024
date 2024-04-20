99471f198c9bd5041ea7b171139555314c98ce72
KristianHedeholm <kristian.hedeholm@gmail.com>
2024-04-20 11:52:53 +0200

    Added A main menu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Change so this works with Rewind.
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //SceneManager.Instance.GetNextScene();
+        }
+    }
+}
