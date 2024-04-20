9aebe0501227231b2296ca3a8e30e961d3300a89
Oliver A. Thunæs <oliver.thunaes@gmail.com>
2024-04-20 11:35:41 +0200

    Added player character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     // Rewired player
     private Player _player;
 
+    private Rigidbody2D _rb2d;
+
     private enum EPlayerState
     {
         Normal,
@@ -17,9 +19,31 @@ public class PlayerController : MonoBehaviour
     private EPlayerState _prevState;
     private EPlayerState _state;
 
+    [SerializeField]
+    private LayerMask collisionMask;
+
+    [SerializeField]
+    private float rideHeight;
+
+    [SerializeField]
+    private float rayDistance;
+
+    [SerializeField]
+    private float rideSpringStrength;
+
+    [SerializeField]
+    private float rideSpringDamp;
+
+    [SerializeField]
+    private float _maxHSpeed;
+
+    [SerializeField]
+    private float _acceleration;
+
     private void Start()
     {
         _player = ReInput.players.GetPlayer(0);
+        _rb2d = GetComponent<Rigidbody2D>();
 
         _prevState = _state = EPlayerState.Normal;
     }
@@ -65,6 +89,8 @@ public class PlayerController : MonoBehaviour
                     break;
             }
         }
+
+        _prevState = _state;
     }
 
     #region Player states
@@ -73,6 +99,37 @@ public class PlayerController : MonoBehaviour
 
     private EPlayerState NormalUpdate()
     {
+        float xMove = _player.GetAxis("Horizontal");
+
+        var speed = _rb2d.velocity;
+        speed.x = Mathf.MoveTowards(speed.x, xMove * _maxHSpeed, _acceleration * Time.deltaTime);
+        _rb2d.velocity = speed;
+
+        if (_player.GetButtonDown("Horizontal") || _player.GetNegativeButtonDown("Horizontal"))
+        {
+            _rb2d.AddForce(Vector2.down * 3f, ForceMode2D.Impulse);
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            Vector3.down,
+            rayDistance,
+            collisionMask
+        );
+
+        if (hit.collider != null)
+        {
+            Vector2 vel = _rb2d.velocity;
+
+            float rayDirVel = Vector2.Dot(Vector2.down, vel);
+
+            float x = hit.distance - rideHeight;
+
+            float springForce = (x * rideSpringStrength) - (rayDirVel * rideSpringDamp);
+
+            _rb2d.AddForce(Vector2.down * springForce);
+        }
+
         return EPlayerState.Normal;
     }
 
